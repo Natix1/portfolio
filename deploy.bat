2886ce759ac61f93f3fbeadb4e314f12cb1158a8
nat1xone <natix1.contact@gmail.com>
2025-08-07 10:54:58 +0200

    Site update

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,8 @@
 @echo off
+set /p commitMsg=Enter commit message: 
+
 git add .
-git commit -m "Update"
+git commit -m "%commitMsg%"
 git push
-ssh vps 'cd /var/www/html && sudo git pull'
+
+ssh vps "cd /var/www/html && sudo git pull"
